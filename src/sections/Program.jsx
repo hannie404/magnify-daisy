22af4923752b3fe065846e780fa3a3900bd2eaeb
hannie404 <annie.business04@gmail.com>
2024-10-08 00:24:29 +0800

    build

--- src/sections/Program.jsx
+++ src/sections/Program.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Program = () => {
   return (
     <div id='program' className='radial-gradient-bg-center md:px-40 md:pb-20 flex flex-col items-center'>
-      <img src="/images/Program.svg" alt="program" className='w-full'/>
+      <img src="/images/Program.svg" alt="program" className='w-full'/> 
     </div>
   )
 }
